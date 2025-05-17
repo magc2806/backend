2ea91d1f6e1ac07359e5e0d8ab90e3fef17d5dfa
magc2806 <gonzalezcabellomanuel@email.com>
2025-05-17 18:34:44 -0400

    feat: add cors config for development. For now we allow all origins
    feat: add devise routes for JWT dispatch & revocation
    
    feat: add JTI column to use JTI matcher revocation strategy
    
    feat: add user serializer
    
    feat: add registrations and sessions controller custom method to handle authentication actions

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
+  include RackSessionsFix
+  respond_to :json
+
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
@@ -59,4 +62,21 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # def after_inactive_sign_up_path_for(resource)
   #   super(resource)
   # end
+
+  def respond_with(resource, _opts = {})
+    if request.method == "POST" && resource.persisted?
+      render json: {
+        status: { code: 200, message: "Signed up sucessfully." },
+        data: UserSerializer.new(resource).serializable_hash[:data][:attributes]
+      }, status: :ok
+    elsif request.method == "DELETE"
+      render json: {
+        status: { code: 200, message: "Account deleted successfully." }
+      }, status: :ok
+    else
+      render json: {
+        status: { code: 422, message: "User couldn't be created successfully. #{resource.errors.full_messages.to_sentence}" }
+      }, status: :unprocessable_entity
+    end
+  end
 end
